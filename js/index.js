8f74595a50033efbad7874963edefc2cfbb73f86
dimasic25 <dimonbubyr@mail.ru>
2021-07-13 16:20:48 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+const baseBackURL = 'http://localhost:8080/users';
+
+const tbody = document.querySelector('tbody');
+
+async function showAllUsers() {
+    let users = await getAllUsers();
+    let i = 1;
+    for (let user of users) {
+        let tr = createTR(user);
+        tr.classList.toggle('row-' + i++);
+        tbody.appendChild(tr);
+    }
+}
+
+async function getAllUsers() {
+    return (await fetchData(baseBackURL));
+}
+
+async function fetchData(url) {
+    let response = await fetch(url);
+    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+                       // получаем тело ответа
+        return await response.json();
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
+}
+
+function createTR(user) {
+    let tr = document.createElement('tr');
+    // id
+    let td1 = document.createElement('td');
+    // First Name
+    let td2 = document.createElement('td');
+    // Last Name
+    let td3 = document.createElement('td');
+    // email
+    let td4 = document.createElement('td');
+
+    td1.textContent = user.id;
+    td2.textContent = user.first_name;
+    td3.textContent = user.last_name;
+    td4.textContent = user.email;
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    return tr;
+}
+
+showAllUsers().then(r => console.log("Успешно!"));
